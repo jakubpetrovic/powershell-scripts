f0fdb5467bf7b7ad0dacaa68fa3b8e3a28784356
Jakub Petrovič <petrovic.jakub@hotmail.com>
2020-08-08 17:54:11 +0200

    add new-labvm script, basic setup

---
+++ hyper-v/new-labvm.ps1
@@ -0,0 +1,16 @@
+#TODO wrap in function
+#TODO add parameters
+#TODO add config file to check vm store, locations etc....
+#TODO setup the config file on first run
+
+$vmPath = "C:\vhd\" 
+$vmName = "placeholder"
+# $vmStartupMemory = 1G
+$vmNewVHDPath = $vmPath + $vmName + "\Virtual Hard Disks\" + $vmName + ".vhdx"
+
+
+#Create VM
+New-VM -Name $vmName -Path $vmPath -MemoryStartupBytes 1024MB -NewVHDPath $vmNewVHDPath -NewVHDSizeBytes 20GB
+
+
+Remove-VM -Name $vmName -Confirm
